a7ea43c4e5089972434f75704c19fa6fa54e812b
shamsherali-devops <shamsherdevp@gmail.com>
2025-09-04 09:32:12 +0500

    done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base :"my_react_app_deploy",
+  base :"/my_react_app_deploy/",
   plugins: [react()],
 })
